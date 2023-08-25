2d867225bb99ece23f44cacfc4f4cd1eb0456def
CarlosDanielGR <carlos.dgr.12@gmail.com>
2023-08-25 15:50:44 -0500

    feat: admin module configured

--- src/app/feature/admin/admin.module.ts
+++ src/app/feature/admin/admin.module.ts
@@ -1,9 +1,13 @@
 import { NgModule } from '@angular/core';
 
+import { NgSimpleSidebarModule } from 'ng-simple-sidebar';
+
+import { AdminRoutingModule } from './admin-routing.module';
 import { AdminComponent } from './admin.component';
+import { SidebarComponent } from './components/sidebar/sidebar.component';
 
 @NgModule({
-  declarations: [AdminComponent],
-  imports: [],
+  declarations: [AdminComponent, SidebarComponent],
+  imports: [NgSimpleSidebarModule, AdminRoutingModule],
 })
 export class AdminModule {}
