8b65b4ff444a1dbf671ac097fc666db53bb6e963
CarlosDanielGR <carlos.dgr.12@gmail.com>
2023-08-25 11:36:55 -0500

    feat: register component created and configured

--- src/app/feature/auth/auth-routing.module.ts
+++ src/app/feature/auth/auth-routing.module.ts
@@ -1,10 +1,24 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+import { AuthComponent } from './auth.component';
+import { RegisterComponent } from './pages/register/register.component';
+
+const routes: Routes = [
+  {
+    path: '',
+    component: AuthComponent,
+    children: [
+      {
+        path: 'register',
+        component: RegisterComponent,
+      },
+    ],
+  },
+];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
+  imports: [RouterModule.forChild(routes)],
   exports: [RouterModule],
 })
 export class AuthRoutingModule {}
--- src/app/feature/auth/auth.module.ts
+++ src/app/feature/auth/auth.module.ts
@@ -1,8 +1,10 @@
 import { NgModule } from '@angular/core';
+import { AuthComponent } from './auth.component';
+import { AuthRoutingModule } from './auth-routing.module';
+import { RegisterComponent } from './pages/register/register.component';
 
 @NgModule({
-  declarations: [],
-  imports: [],
-  providers: [],
+  declarations: [AuthComponent, RegisterComponent],
+  imports: [AuthRoutingModule],
 })
 export class AuthModule {}
