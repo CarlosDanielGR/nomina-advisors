f86efcfd1123fad279e59c147452e530606a88d0
CarlosDanielGR <carlos.dgr.12@gmail.com>
2023-08-26 14:08:11 -0500

    feat: header component created

---
+++ src/app/core/components/header/header.component.ts
@@ -0,0 +1,17 @@
+import { Component, OnInit } from '@angular/core';
+import { environment } from 'src/environments/environment';
+
+@Component({
+  selector: 'app-header',
+  templateUrl: './header.component.html',
+  styleUrls: ['./header.component.scss'],
+})
+export class HeaderComponent implements OnInit {
+  readonly ASSETS_LOGO = environment.ASSETS_LOGO;
+
+  currentRoute: string = '';
+
+  constructor() {}
+
+  ngOnInit(): void {}
+}
