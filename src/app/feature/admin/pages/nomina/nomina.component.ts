01048d16b3f51049432a60f3e442e2a5bda16b9c
CarlosDanielGR <carlos.dgr.12@gmail.com>
2023-08-28 14:48:07 -0500

    feat: nomina adviser partial completed

--- src/app/feature/admin/pages/nomina/nomina.component.ts
+++ src/app/feature/admin/pages/nomina/nomina.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { User } from '../../interfaces/profile.interface';
 import { Observable } from 'rxjs';
 import { AdminService } from '../../services/admin.service';
+import { TYPE_ADVISER } from 'src/app/shared/constant/comission.constant';
 
 @Component({
   selector: 'app-nomina',
@@ -10,6 +11,8 @@ import { AdminService } from '../../services/admin.service';
 })
 export class NominaComponent implements OnInit {
   nominaData$: Observable<User[]> = new Observable();
+
+  typeAdviser = TYPE_ADVISER;
 
   constructor(private readonly adminService: AdminService) {}
 
