12541f5d35949d7a97c133ed7691e4765b693f84
CarlosDanielGR <carlos.dgr.12@gmail.com>
2023-08-29 10:39:57 -0500

    feat: sales completed

--- src/app/feature/admin/pages/sales/components/sale-simulator/sale-simulator.component.ts
+++ src/app/feature/admin/pages/sales/components/sale-simulator/sale-simulator.component.ts
@@ -1,8 +1,8 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+
 import { NgbActiveModal } from '@ng-bootstrap/ng-bootstrap';
-import { Observable } from 'rxjs';
-import { User } from 'src/app/feature/admin/interfaces/profile.interface';
+
 import { AdminService } from 'src/app/feature/admin/services/admin.service';
 
 @Component({
@@ -11,18 +11,17 @@ import { AdminService } from 'src/app/feature/admin/services/admin.service';
   styleUrls: ['./sale-simulator.component.scss'],
 })
 export class SaleSimulatorComponent implements OnInit {
-  users$: Observable<User[]> = new Observable();
+  @Input() users: Record<string, string>[] = [];
 
   formSale!: FormGroup;
 
   constructor(
-    private readonly adminService: AdminService,
     private readonly formBuilder: FormBuilder,
-    private readonly activeModal: NgbActiveModal
+    private readonly activeModal: NgbActiveModal,
+    private readonly adminService: AdminService
   ) {}
 
   ngOnInit(): void {
-    this.users$ = this.adminService.getAllUsers();
     this.initFormSale();
   }
 
--- src/app/feature/admin/pages/sales/sales.component.ts
+++ src/app/feature/admin/pages/sales/sales.component.ts
@@ -1,6 +1,9 @@
 import { Component, OnInit } from '@angular/core';
+
 import { NgbModal } from '@ng-bootstrap/ng-bootstrap';
+
 import { SaleSimulatorComponent } from './components/sale-simulator/sale-simulator.component';
+import { AdminService } from '../../services/admin.service';
 
 @Component({
   selector: 'app-sales',
@@ -8,11 +11,51 @@ import { SaleSimulatorComponent } from './components/sale-simulator/sale-simulat
   styleUrls: ['./sales.component.scss'],
 })
 export class SalesComponent implements OnInit {
-  constructor(private readonly modalService: NgbModal) {}
+  private users: Record<string, string>[] = [];
 
-  ngOnInit(): void {}
+  allSales: Record<string, string | number>[] = [];
+
+  firstLoad: boolean = false;
+
+  constructor(
+    private readonly modalService: NgbModal,
+    private readonly adminService: AdminService
+  ) {}
+
+  ngOnInit(): void {
+    this.getAllDataUser();
+  }
+
+  private getAllDataUser(): void {
+    this.adminService.getAllUsers().subscribe({
+      next: (res) => {
+        if (!this.firstLoad)
+          this.users = res.map((user) => ({
+            name: user.name,
+            id: user.id,
+          }));
+        this.allSales = res.map((user) => ({
+          name: user.name,
+          sales: user.sales
+            .map((sale) => sale.price)
+            .reduce((sum, num) => {
+              return +sum + +num;
+            }, 0),
+        }));
+        this.firstLoad = true;
+      },
+    });
+  }
 
   openSaleSimulator(): void {
-    this.modalService.open(SaleSimulatorComponent, { centered: true });
+    const modalRef = this.modalService.open(SaleSimulatorComponent, {
+      centered: true,
+    });
+    modalRef.componentInstance.users = this.users;
+    modalRef.closed.subscribe({
+      next: () => {
+        this.getAllDataUser();
+      },
+    });
   }
 }
